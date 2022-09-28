5798d5a3ad8bc2ee8010a5b67a82c5c5b051c533
fengzero <fengzero@fengzero.com>
2022-09-28 06:36:20 +0000

    update install.sh

--- install.sh
+++ install.sh
@@ -5,17 +5,21 @@ set -e
 install_dir=
 arch=x86_64
 compile=
+library=
 
-while getopts ":d:a:c:x:" OPT; do
+while getopts ":d:a:c:b:" OPT; do
     case ${OPT} in
         a)
             arch=$OPTARG
             ;;
-        d)
-            install_dir=$OPTARG
+        b)
+            library=$OPTARG
             ;;
         c)
             compile=$OPTARG
+            ;;
+        d)
+            install_dir=$OPTARG
             ;;
     esac
 done
@@ -23,15 +27,10 @@ done
 gcc_compile=${compile}-gcc
 gxx_compile=${compile}-g++
 
-#install_dir=${dir}/${compiler_prefix}
-
-#$1 rep
-#$2 tag
-#$3 compile option
 function compile_source() {
-    git clone https://github.com/$1 tmp
-    cd tmp
-    git checkout -b br $2
+    cd $library
+    git clone https://github.com/$1
+    cd $2
     mkdir build && cd build
     cmake .. -DCMAKE_C_COMPILER=${gcc_compile} \
         -DCMAKE_CXX_COMPILER=${gxx_compile} \
@@ -40,16 +39,28 @@ function compile_source() {
         $3
     # github-hosted runners has 2 core
     make -j4 && sudo make install
-    cd ../../
-    rm -rf tmp
+}
+
+function compile_source_with_tag() {
+    cd $library
+    git clone -b $3 https://github.com/$1 $2
+    cd $2
+    mkdir build && cd build
+    cmake .. -DCMAKE_C_COMPILER=${gcc_compile} \
+        -DCMAKE_CXX_COMPILER=${gxx_compile} \
+        -DCMAKE_STAGING_PREFIX=${install_dir} \
+        -DCMAKE_PREFIX_PATH=${install_dir} \
+        $4
+    # github-hosted runners has 2 core
+    make -j4 && sudo make install
 }
 
 function build_openssl() {
     echo "Installing openssl (1.1.1)"
+    cd $library
     if [ $arch == "x86_64" ]; then
         sudo apt-get install -y openssl libssl-dev
     else
-
         git clone -b OpenSSL_1_1_1 https://github.com/openssl/openssl.git
         cd openssl
         mkdir -p ${install_dir}/openssl/ssl
@@ -61,11 +72,11 @@ function build_openssl() {
         make -j4
         make install_sw
         make clean
-        cd ../
     fi
 }
 
 function build_zlog() {
+    cd $library
     git clone -b 1.2.15 https://github.com/HardySimpson/zlog.git
     cd zlog
     echo ${gcc_compile}
@@ -79,6 +90,7 @@ function build_zlog() {
 }
 
 function build_sqlite3() {
+    cd $library
     curl https://www.sqlite.org/2022/sqlite-autoconf-3390000.tar.gz \
       --output sqlite3.tar.gz
     mkdir -p sqlite3
@@ -89,17 +101,31 @@ function build_sqlite3() {
                 --host ${arch} CC=${gcc_compile} \
       && make -j4 \
       && sudo make install
-    cd ../
+}
+
+function install_protobuf-c() {
+    cd $library
+    echo "Install protobuf-c(1.4)"
+    git clone https://github.com/neugates/neuron-build.git
+    cd neuron-build
+    sudo cp -r ${arch}/protobuf-c/include/* ${install_dir}/include/
+    sudo cp -r ${arch}/protobuf-c/lib/* ${install_dir}/lib/
+    echo "End install protobuf-c(1.4)"
 }
 
 build_zlog
-compile_source neugates/jansson.git HEAD "-DJANSSON_BUILD_DOCS=OFF -DJANSSON_EXAMPLES=OFF"
 build_openssl 
-compile_source neugates/nng.git HEAD "-DBUILD_SHARED_LIBS=OFF -DNNG_TESTS=OFF"
-compile_source benmcollins/libjwt.git v1.13.1 "-DENABLE_PIC=ON -DBUILD_SHARED_LIBS=OFF"
-compile_source neugates/MQTT-C.git HEAD "-DCMAKE_POSITION_INDEPENDENT_CODE=ON -DMQTT_C_OpenSSL_SUPPORT=ON -DMQTT_C_EXAMPLES=OFF"
 build_sqlite3
+install_protobuf-c
+
+compile_source neugates/jansson.git jansson "-DJANSSON_BUILD_DOCS=OFF -DJANSSON_EXAMPLES=OFF"
+compile_source neugates/nng.git nng "-DBUILD_SHARED_LIBS=OFF -DNNG_TESTS=OFF"
+compile_source neugates/MQTT-C.git MQTT-C "-DCMAKE_POSITION_INDEPENDENT_CODE=ON -DMQTT_C_OpenSSL_SUPPORT=ON -DMQTT_C_EXAMPLES=OFF"
+
+compile_source_with_tag benmcollins/libjwt.git libjwt v1.13.1 "-DENABLE_PIC=ON -DBUILD_SHARED_LIBS=OFF"
+compile_source_with_tag ARMmbed/mbedtls.git mbedtls v2.16.12 "-DCMAKE_BUILD_TYPE=Release -DUSE_SHARED_MBEDTLS_LIBRARY=OFF -DENABLE_TESTING=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON"
+compile_source_with_tag open62541/open62541.git open62541 v1.0.6 "-DBUILD_SHARED_LIBS=OFF -DUA_ENABLE_AMALGAMATION=ON -DUA_ENABLE_ENCRYPTION=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DUA_LOGLEVEL=100"
 
 if [ $arch == "x86_64" ]; then
-    compile_source google/googletest.git release-1.11.0
+    compile_source_with_tag google/googletest.git googletest release-1.11.0
 fi
