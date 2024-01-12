25462d9f74fb9e0fad77c90e1111ad9a16ecb4fc
fengzeroz <53388969+fengzeroz@users.noreply.github.com>
2024-01-12 10:05:58 +0800

    Update cross-package.sh

--- cross-package.sh
+++ cross-package.sh
@@ -116,10 +116,10 @@ cp $neuron_modules_dir/build/plugins/libplugin-websocket.so \
 		$neuron_modules_dir/build/plugins/libplugin-srtp.so \
     	$package_dir/plugins/
 
-#cp $neuron_modules_dir/build/plugins/libplugin-gewu2.so \
-        #$neuron_modules_dir/build/plugins/libplugin-s7comm-for-un.so \
-		#$neuron_modules_dir/build/plugins/libplugin-focas.so \
-   		#$package_dir/plugins/
+cp $neuron_modules_dir/build/plugins/libplugin-gewu2.so \
+        $neuron_modules_dir/build/plugins/libplugin-s7comm-for-un.so \
+		$neuron_modules_dir/build/plugins/libplugin-focas.so \
+   		$package_dir/plugins/
 
 cp $neuron_modules_dir/build/plugins/focas/libfocas32.so.1 $package_dir/
 
