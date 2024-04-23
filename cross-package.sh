c28b656fc0ab336d4d6d6efc60f1bb8d15cf416e
eeff <33566201+eeff@users.noreply.github.com>
2024-04-23 11:46:39 +0800

    package AWS IoT

--- cross-package.sh
+++ cross-package.sh
@@ -80,6 +80,7 @@ cp $neuron_modules_dir/default_plugins.json \
 
 cp $neuron_dir/build/plugins/libplugin-mqtt.so \
     $neuron_dir/build/plugins/libplugin-ekuiper.so \
+    $neuron_dir/build/plugins/libplugin-aws-iot.so \
     $package_dir/plugins/
 
 cp $neuron_dir/build/plugins/schema/*.json \
