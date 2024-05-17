ca0949b57bcbfe187914c5d3533f71d0f265a979
fengzero <fengzero@fengzero.com>
2024-05-17 02:53:53 +0000

    branch v2.9

--- create_neuron_deb.py
+++ create_neuron_deb.py
@@ -16,7 +16,7 @@ def parse_args():
 args = parse_args()
 
 home = '/home/neuron'
-branch = '/main'
+branch = '/v2.9'
 package_dir = home + branch + '/Program/' + args.vendor + '/package/neuron'
 rules = []
 
