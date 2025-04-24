5b0e4d3b801f52fe08ea14fe30b71bf08d3a5d6b
xiang.zhou <xiang.zhou@emqx.io>
2025-04-24 11:15:41 +0800

    package neuhub so file

--- cross-package.sh
+++ cross-package.sh
@@ -161,6 +161,7 @@ cp $neuron_modules_dir/build/plugins/libplugin-websocket.so \
         $neuron_modules_dir/build/plugins/libplugin-ab5000.so \
         $neuron_modules_dir/build/plugins/libplugin-omron-cip.so \
         $neuron_modules_dir/build/plugins/libplugin-dnp3.so \
+		$neuron_modules_dir/build/plugins/libplugin-neuhub.so \
         $package_dir/plugins/
 
 cp $neuron_modules_dir/build/plugins/schema/*.json \
